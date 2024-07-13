759b12cf72d1153c3cfaa8f93a70f563dad3d2f0
zhboat <zhboat@163.com>
2024-07-13 17:35:06 +0800

    fix: modify var name in 88.py

--- 88.py
+++ 88.py
@@ -14,24 +14,24 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1, p2, t = m - 1, n - 1, m + n - 1
-
+        p1 = m - 1
+        p2 = n - 1
+        p3 = m + n - 1
         while p1 >= 0 and p2 >= 0:
             if nums1[p1] > nums2[p2]:
-                nums1[t] = nums1[p1]
+                nums1[p3] = nums1[p1]
                 p1 -= 1
             else:
-                nums1[t] = nums2[p2]
+                nums1[p3] = nums2[p2]
                 p2 -= 1
-            t -= 1
-
+            p3 -= 1
+        
         while p2 >= 0:
-            nums1[t] = nums2[p2]
+            nums1[p3] = nums2[p2]
             p2 -= 1
-            t -= 1
+            p3 -= 1
 
-
-n1 = [1, 2, 3, 0, 0, 0]
-n2 = [2, 5, 6]
-Solution().merge(n1, 3, n2, 3)
+n1 = [0]
+n2 = [1]
+Solution().merge(n1, 0, n2, 1)
 print(n1)
